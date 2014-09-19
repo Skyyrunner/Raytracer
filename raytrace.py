ba1b1b9a3efdc7379bd6ddb5c7f4edb2056acd80
Skyyrunner <fluffyterran@gmail.com>
2014-09-19 14:02:00 +0900

    Added simple diffuse lighting. Compares light vector and normal vector for brightness. Next up is a camera overhaul.

--- raytrace.py
+++ raytrace.py
@@ -9,12 +9,16 @@ class RayColor:
         self.rgb = rgb
         
     def toRGB(self):
-        return (int(self.rgb[0]), int(self.rgb[1]), int(self.rgb[2]))
+        i = self.intensity
+        return (int(self.rgb[0]*i), int(self.rgb[1]*i), int(self.rgb[2]*i))
 
 class RaycastingObject(object):
     def __init__(self):
         self.color = (255,255,0)
         self.reflectionIndex = 0.0
+    
+    def getColor(self, coords=None):
+        return self.color
 
     def intersect(self, ray):
         raise BaseException("Must overload intersect() in class " + type(self).__name__)
@@ -54,6 +58,37 @@ class RaycastingSphere(RaycastingObject):
     def __ne__(self, other):
         return not self.__eq__(other)
 
+class RaycastingPlane(RaycastingObject):
+    def __init__(self, plane):
+        super(RaycastingPlane, self).__init__()
+        if type(plane)!=euclid.Plane:
+            raise TypeError("Requires Plane")
+        self.shape = plane
+        self.color1 = (0,0,0)
+        self.color2 = (200,200,200)
+
+    def intersect(self, ray):
+        return self.shape.intersect(ray)
+
+    def distance(self, ray):
+        return self.shape.distance(ray)
+
+    def getColor(self, coords=None):
+        s = int(coords[0]) + int(coords[1]) + int(coords[2])
+        if s % 2 == 0:
+            return self.color1
+        return self.color2
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        a = self.n == other.n
+        b = self.k == other.k
+        return a and b
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 distances = []
 
 def reflect(point, obj, ray):
@@ -66,11 +101,14 @@ def reflect(point, obj, ray):
 
 # Supply 2 RayColors, returns RGB tuple that must be floor()'d before used in PIL
 def blend(colors):
+    intensity = 0.0
+    for c in colors:
+        intensity += c.intensity
     colors.insert(0, 0.0)
     rgb = [0,0,0]
     for x in xrange(3):
         rgb[x] = reduce(lambda s, c: s + c.rgb[x] * c.intensity, colors)   
-    return tuple(rgb)
+    return RayColor(intensity, rgb) 
 
 class Scene:
     def __init__(self):
@@ -79,9 +117,9 @@ class Scene:
         # for now, directly define screen
         self.topleft = euclid.Point3(20.0, -4.0, -4.0)
         self.bottomright = euclid.Point3(20.0, -12.0, -12.0)
-        self.camera = euclid.Point3(100.0, 0.0, 0.0)
+        self.camera = euclid.Point3(30.0, 0.0, 0.0)
         # light source is a single point for now
-        self.light = euclid.Point3(-50.0, 0.0, 0.0)
+        self.light = euclid.Point3(-50.0,30.0,5.0)
 
 
     def getPixelCoords(self, w, h):
@@ -93,21 +131,35 @@ class Scene:
                 yield x, y, euclid.Point3(self.topleft.x, self.topleft.y + deltaW * x, self.topleft.z + deltaH * y)
 
     def getColor(self, intersect, obj, intensity):
+        # DEBUG: no shadows
+        #return RayColor(intensity, obj.getColor(intersect))
         # get a ray from the intersect to the source of light
         vectorToLight = self.light - intersect
         rayToLight = euclid.Ray3(intersect, vectorToLight)
         for o in self.objects:
             i = o.intersect(rayToLight)
             if obj == o:
+                if type(i) == euclid.Point3:
+                    # often means intercept with self
+                    continue
+                elif i == None:
+                    continue # no intercept
                 lenI = abs(i)
                 distances.append(lenI)
                 if lenI < 0.1:
                     continue
-            if i != None:
+            if type(i) != None:
                 # is in shadow
                 return RayColor(intensity, (0,0,0))
         # otherwise not in shadow
-        return RayColor(intensity, obj.color)
+        # find intensity depending on angle to light.
+        if type(obj) == RaycastingPlane:
+            normal = obj.shape.n
+        elif type(obj) == RaycastingSphere:
+            normal = (intersect - obj.shape.c).normalize()
+        strength = abs(vectorToLight.normalized().dot(normal))
+        #strength = 1.0
+        return RayColor(intensity*strength, obj.getColor(intersect))
 
     def findIntersect(self, ray):
         maxD = 0
@@ -115,19 +167,26 @@ class Scene:
         obj = None
         for o in self.objects:
             inter = o.intersect(ray)
-            if inter != None:
+            if type(inter)!= None:
                 # intersects
-                d = abs(ray.p - o.c)
-                if d > maxD:
-                    maxD = d
-                    obj = o
-                    # inter is a line segment. We require the closer point.
-                    L1 = abs(ray.p - inter.p1)
-                    L2 = abs(ray.p - inter.p2)
-                    if L1 > L2:
-                        intersect = inter.p1
-                    else:
-                        intersect = inter.p2
+                if type(inter) == euclid.LineSegment3:
+                    d = abs(ray.p - o.c)
+                    if d > maxD:
+                        maxD = d
+                        obj = o
+                        # if inter is a line segment. We require the closer point.
+                        L1 = abs(ray.p - inter.p1)
+                        L2 = abs(ray.p - inter.p2)
+                        if L1 > L2:
+                            intersect = inter.p1
+                        else:
+                            intersect = inter.p2
+                elif type(inter) == euclid.Point3:
+                    d = abs(inter - ray.p)
+                    if d > maxD:
+                        maxD = d
+                        obj = o
+                        intersect = inter
                         
         if obj == None:
             return None,None
@@ -151,9 +210,10 @@ class Scene:
 
 if __name__=="__main__":
     #import rpdb2; rpdb2.start_embedded_debugger('1234')
-    w = 180
+    w = 360
     scene = Scene()
     scene.objects.append(RaycastingSphere(euclid.Point3(0,0,0), 5.0))
+    #scene.objects.append(RaycastingPlane(euclid.Plane(euclid.Point3(1,1,1), euclid.Vector3(0.0,1.0,1.0))))
     im = Image.new("RGB", (w, w), (0,0,255))
     pixels = im.load()
     for x, y, point in scene.getPixelCoords(w, w):
@@ -164,9 +224,9 @@ if __name__=="__main__":
             pixels[x,y] = color
         else:
             raise TypeError("unknown color type: " + str(type(color)) + " containing:\n" + repr(color))
-    
-    print "average:" + str(reduce(lambda x,y: x + y, distances)/float(len(distances)))
-    print "min:" + str(reduce(lambda x,y: x if x < y else y, distances))
-    distances.insert(0, 0.0)
-    print "number of 0.0s:" + str(reduce(lambda x,y: x+1 if y == 0.0 else x, distances))
+    if not len(distances) < 2:
+        print "average:" + str(reduce(lambda x,y: x + y, distances)/float(len(distances)))
+        print "min:" + str(reduce(lambda x,y: x if x < y else y, distances))
+        distances.insert(0, 0.0)
+        print "number of 0.0s:" + str(reduce(lambda x,y: x+1 if y == 0.0 else x, distances))
     im.save("/home/skyrunner/upload/imgs/test.png")
